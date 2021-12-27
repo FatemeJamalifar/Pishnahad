e9807957a3cede398655b3641e1d2aa37bece12a
Fatemeh Jamalifar <96416423+FatemehJamalifar@users.noreply.github.com>
2021-12-27 20:56:22 +0330

    create test for blog application and install selenium

--- Blog/tests.py
+++ Blog/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+
+from Blog.models import Post
+
+class SeleniumTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        ### set config for selenium
+        super().setUpClass()
+        cls.path = os.path.dirname(os.path.abspath(__file__))
+        cls.address = os.path.join(cls.path, "chromedriver")
+        cls.driver = webdriver.Chrome(executable_path=cls.address)
+        cls.driver.delete_all_cookies()
+        cls.driver.get('http://127.0.0.1:8000/login/')
+        cls.posts = Post.objects.all()
+
+    @classmethod
+    def tearDownClass(cls):
+        ### distroying
+        cls.driver.quit()
+        super().tearDownClass()
+
+    def test_course_created(self):
+        self.driver.
